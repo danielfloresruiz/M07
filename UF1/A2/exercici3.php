3cd3bc44d5d29c90acdfbc48c7e3f2b6639ee2cb
danielfloresruiz <dfloresr@fp.insjoaquimmir.cat>
2020-10-04 13:51:37 +0200

    calendari

--- UF1/A2/exercici3.php
+++ UF1/A2/exercici3.php
@@ -4,20 +4,21 @@ $cantolumnas=7;
 $dias=array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
 
 echo "<table border=1>";
-for ($i=0;$i<count(dias);i++) {
-    echo "<tr>";
-    for ($col=1;$col<=$cantolumnas;$col++)
-    {
-        echo "<td> $dais[col] </td>";
-    };
-    echo "</tr>"
-}
-for ($fila=1;$fila<=$cantfilas;$fila++)
+
+echo "<tr>";
+for ($col=0;$col<=$cantolumnas-1;$col++)
 {
+    echo "<td> $dias[$col] </td>";
+};
+echo "</tr>";
+
+$num=1;
+for ($fila=2;$fila<=$cantfilas;$fila++) {
     echo "<tr>";
     for ($col=1;$col<=$cantolumnas;$col++)
     {
-        echo "<td> Fila $fila, columna $col </td>";
+        echo "<td> $num </td>";
+	$num++;
     };
     echo "</tr>";
 };
