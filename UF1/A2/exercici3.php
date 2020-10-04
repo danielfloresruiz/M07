6c716f624e94abb272a2f8bebad4909ef576f546
danielfloresruiz <72168262+danielfloresruiz@users.noreply.github.com>
2020-10-04 13:36:14 +0200

    Update exercici3.php

--- UF1/A2/exercici3.php
+++ UF1/A2/exercici3.php
@@ -4,7 +4,14 @@ $cantolumnas=7;
 $dias=array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
 
 echo "<table border=1>";
-
+for ($i=0;$i<count(dias);i++) {
+    echo "<tr>";
+    for ($col=1;$col<=$cantolumnas;$col++)
+    {
+        echo "<td> $dais[col] </td>";
+    };
+    echo "</tr>"
+}
 for ($fila=1;$fila<=$cantfilas;$fila++)
 {
     echo "<tr>";
@@ -15,5 +22,4 @@ for ($fila=1;$fila<=$cantfilas;$fila++)
     echo "</tr>";
 };
 echo "</table>";
-echo "hola"
 ?>
