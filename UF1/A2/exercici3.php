db96cd6a01997c07576cb8c8a20eb11868f7c18a
danielfloresruiz <dfloresr@fp.insjoaquimmir.cat>
2020-10-05 17:22:24 +0200

    caledari

--- UF1/A2/exercici3.php
+++ UF1/A2/exercici3.php
@@ -1,26 +1,48 @@
 <?php
-$cantfilas=6;
-$cantolumnas=7;
+
+# definimos los valores iniciales para nuestro calendario
+$mes=date("n");
+$año=date("Y");
+$diaActual=date("j");
+ 
+# Obtenemos el dia de la semana del primer dia
+# Devuelve 0 para domingo, 6 para sabado
+$diaSemana=date("w",mktime(0,0,0,$mes,1,$año))+7;
+# Obtenemos el ultimo dia del mes
+$ultimoDiaMes=date("d",(mktime(0,0,0,$mes+1,1,$año)-1));
+
 $dias=array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
+
 
 echo "<table border=1>";
 
-echo "<tr>";
-for ($col=0;$col<=$cantolumnas-1;$col++)
-{
-    echo "<td> $dias[$col] </td>";
-};
-echo "</tr>";
-
-$num=1;
-for ($fila=2;$fila<=$cantfilas;$fila++) {
-    echo "<tr>";
-    for ($col=1;$col<=$cantolumnas;$col++)
+    for ($i=0;$i<=6;$i++)
     {
-        echo "<td> $num </td>";
-	$num++;
+        echo "<td> $dias[$i] </td>";
     };
     echo "</tr>";
-};
-echo "</table>";
+    
+	
+	$last_cell=$diaSemana+$ultimoDiaMes;
+	
+	for($i=1;$i<=42;$i++)
+	{
+		if($i==$diaSemana)
+		{
+			$dia=1;
+		}
+		if($i<$diaSemana || $i>=$last_cell)
+		{
+			echo "<td>&nbsp;</td>";
+		}else{
+			echo "<td>$dia</td>";
+			$dia++;
+		}
+		if($i%7==0)
+		{
+			echo "</tr><tr>\n";
+        }
+    }
+    
+echo "</table>"
 ?>
